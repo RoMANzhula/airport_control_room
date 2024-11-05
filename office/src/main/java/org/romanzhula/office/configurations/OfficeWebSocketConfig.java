82ef5661b51b62a3e48e22e80aeb33a22265c861
RoMANzhula <roman1985al@ukr.net>
2024-11-05 22:08:34 +0200

    Add OfficeWebSocketConfig - office modul

---
+++ office/src/main/java/org/romanzhula/office/configurations/OfficeWebSocketConfig.java
@@ -0,0 +1,31 @@
+package org.romanzhula.office.configurations;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.office.handlers.OfficeSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import utils.helpers.MessageConverter;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class OfficeWebSocketConfig implements WebSocketConfigurer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final MessageConverter messageConverter;
+    private final Cache<String, WebSocketSession> sessionCache;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(new OfficeSocketHandler(kafkaTemplate, messageConverter, sessionCache))
+                .setAllowedOrigins("*")
+        ;
+    }
+
+}
